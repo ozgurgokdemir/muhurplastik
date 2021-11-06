22262c9f5c9ec48272ec1fcc6bc0e742c18af20c
Rawthul <ozgurgokd@gmail.com>
2021-11-07 02:06:41 +0300

    navigation & dropdown changes

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 const fontSize = window.getComputedStyle(document.documentElement).getPropertyValue('font-size').replace('px', '');
 
 const breakpoints = {
+  small: fontSize * 30,
   medium: fontSize * 48,
   large: fontSize * 62,
   xlarge: fontSize * 75
@@ -9,7 +10,7 @@ const breakpoints = {
 let windowWidth = window.innerWidth;
 
 let isMediaMobile = () => {
-  return (windowWidth < breakpoints.medium) ? true : false;
+  return (windowWidth < breakpoints.large) ? true : false;
 }
 
 let currentMedia = isMediaMobile();
@@ -24,48 +25,31 @@ let isActive = (target) => {
 
 window.addEventListener('resize', () => {
   windowWidth = window.innerWidth;
-  BreakpointResets();
+  if (isBreakPoint()) BreakpointResets();
 });
 
 function BreakpointResets() {
-  if (isBreakPoint()) {
-    currentMedia = isMediaMobile();
-    if (windowWidth >= breakpoints.medium) {
-      // reset hamburger menu
-      hamburgerMenu.classList.remove('active');
-      hamburgerMenu.style.removeProperty('transform');
-      // reset dropdown
-      dropdownContainer.classList.remove('active');
-      navDropdown.style.height = 'auto';     
-      dropdownHeight = navDropdown.clientHeight + 'px';
-      navDropdown.style.removeProperty('height');
-      // reset dropdown content for desktop
-      dropdownContent.style.height = 'auto';     
-      dropdownContentHeight = dropdownContent.clientHeight + 'px';
-      dropdownContentPadding = {
-        top: window.getComputedStyle(dropdownContent).getPropertyValue('padding-top'),
-        bottom: window.getComputedStyle(dropdownContent).getPropertyValue('padding-bottom')
-      }
-      dropdownContent.style.removeProperty('height');
-      // reset hamburger button
-      btnHamburger.querySelectorAll('span').forEach((bar) => {
-        bar.style.removeProperty('transform');
-        bar.style.removeProperty('opacity');
-      });
-    } else {
-      // reset dropdown for mobile
-      navDropdown.style.height = 'auto';     
-      dropdownHeight = navDropdown.clientHeight + 'px';
-      navDropdown.style.removeProperty('height');
-      // reset dropdown content for mobile
-      dropdownContent.style.height = 'auto';     
-      dropdownContentHeight = dropdownContent.clientHeight + 'px';
-      dropdownContentPadding = {
-        top: window.getComputedStyle(dropdownContent).getPropertyValue('padding-top'),
-        bottom: window.getComputedStyle(dropdownContent).getPropertyValue('padding-bottom')
-      }
-      dropdownContent.style.removeProperty('height');
-    }
+  currentMedia = isMediaMobile();
+  if (windowWidth >= breakpoints.large) {
+    // reset hamburger menu
+    hamburgerMenu.classList.remove('active');
+    hamburgerMenu.style.removeProperty('transform');
+    // reset hamburger button
+    btnHamburger.querySelectorAll('span').forEach((bar) => {
+      bar.style.removeProperty('transform');
+      bar.style.removeProperty('opacity');
+    });
+    // reset dropdown for desktop
+    dropdownDesktop.classList.remove('active');
+    dropdownDesktop.style.height = 'auto';     
+    dropdownDesktopHeight = dropdownDesktop.clientHeight + 'px';
+    dropdownDesktop.style.removeProperty('height');
+  } else {
+    // reset dropdown for mobile
+    dropdownMobile.classList.remove('active');
+    dropdownMobile.style.height = 'auto';     
+    dropdownMobileHeight = dropdownMobile.clientHeight + 'px';
+    dropdownMobile.style.removeProperty('height');
   }
 }
 
@@ -75,65 +59,36 @@ function BreakpointResets() {
 
 const btnHamburger = document.getElementById("btnHamburger");
 const hamburgerMenu = document.getElementById("hamburgerMenu");
+const body = document.body;
 
+// toggle menu when clicked to hamburger button
 btnHamburger.addEventListener('click', () => {
-  if (windowWidth < breakpoints.medium) {
-    if (!isActive(hamburgerMenu)) {
-      hamburgerMenu.classList.add('active');
-      MenuToggle(hamburgerMenu, 300);
-      HamburgerToggle(btnHamburger, 300);
-    } else {
-      hamburgerMenu.classList.remove('active');
-      MenuToggle(hamburgerMenu, 300);
-      HamburgerToggle(btnHamburger, 300);
-    }
+  if (!isActive(hamburgerMenu)) {
+    hamburgerMenu.classList.add('active');
+    body.classList.add('noscroll');
+    HamburgerToggle(btnHamburger, 300);
+  } else {
+    hamburgerMenu.classList.remove('active');
+    body.classList.remove('noscroll');
+    HamburgerToggle(btnHamburger, 300);
   }
 });
 
+// close menu when clicked to overlay
 window.addEventListener('click', (event) => {
-  if (windowWidth < breakpoints.medium) {
-    if (event.target == hamburgerMenu) {
-      hamburgerMenu.classList.remove('active');
-      MenuToggle(hamburgerMenu, 300);
-      HamburgerToggle(btnHamburger, 300);
-    }
+  if (event.target == hamburgerMenu) {
+    hamburgerMenu.classList.remove('active');
+    body.classList.remove('noscroll');
+    HamburgerToggle(btnHamburger, 300);
   }
 });
 
 let lastMenuToggle = [];
-function MenuToggle(target, duration=300) {
-  if (isActive(hamburgerMenu)) {
-    let transitionNumber = lastMenuToggle.length;
-    lastMenuToggle.push(transitionNumber);
-    target.style.transitionProperty = "transform";
-    target.style.transitionDuration = duration + 'ms';
-    target.style.transitionTimingFunction = 'ease-in-out';
-    target.style.transform = 'translateX(0)';
-    window.setTimeout( () => {
-      if (isActive(hamburgerMenu) && lastMenuToggle[lastMenuToggle.length - 1] == transitionNumber) {
-        target.style.removeProperty('transition');
-      }
-    }, duration);
-  } else {
-    let transitionNumber = lastMenuToggle.length;
-    lastMenuToggle.push(transitionNumber);
-    target.style.transitionProperty = "transform";
-    target.style.transitionDuration = duration + 'ms';
-    target.style.transitionTimingFunction = 'ease-in-out';
-    target.style.transform = 'translateX(-100%)';
-    window.setTimeout( () => {
-      if (!isActive(hamburgerMenu) && lastMenuToggle[lastMenuToggle.length - 1] == transitionNumber) {
-        target.style.removeProperty('transition');
-        target.style.removeProperty('transform');
-      }
-    }, duration);
-  }
-}
-
 function HamburgerToggle(target, duration=300) {
   bars = target.querySelectorAll('span');
   if (isActive(hamburgerMenu)) {
-    let transitionNumber = lastMenuToggle.length - 1;
+    let transitionNumber = lastMenuToggle.length;
+    lastMenuToggle.push(transitionNumber);
     for (let i = 0; i < bars.length; i++) {
       bars[i].style.transitionProperty = "transform, opacity";
       bars[i].style.transitionDuration = duration / 2 + 'ms';
@@ -150,11 +105,13 @@ function HamburgerToggle(target, duration=300) {
     window.setTimeout(() => {
       if (isActive(hamburgerMenu)) {
         bars[0].style.transform = 'translateY(300%) rotate(-45deg)';
+        bars[1].style.opacity = '0';
         bars[2].style.transform = 'translateY(-300%) rotate(45deg)';
       }
     }, duration / 2);
   } else {
-    let transitionNumber = lastMenuToggle.length - 1;
+    let transitionNumber = lastMenuToggle.length;
+    lastMenuToggle.push(transitionNumber);
     for (let i = 0; i < bars.length; i++) {
       bars[i].style.transitionProperty = "transform, opacity";
       bars[i].style.transitionDuration = duration / 2 + 'ms';
@@ -179,82 +136,91 @@ function HamburgerToggle(target, duration=300) {
 
 /* DROPDOWN */
 
-const dropdownContainer = document.querySelector('.nav__item--dropdown');
-const btnDropdown = document.getElementById("btnDropdown");
-const navDropdown = document.getElementById("navDropdown");
-const dropdownContent = document.querySelector('.dropdown__content');
+const dropdownDesktopContainer = document.getElementById("dropdownDesktopContainer");
+const btnDropdownMobile = document.getElementById("btnDropdownMobile");
+const btnDropdownDesktop = document.getElementById("btnDropdownDesktop");
+const dropdownMobile = document.getElementById("dropdownMobile");
+const dropdownDesktop = document.getElementById("dropdownDesktop");
 
 // dropdown toggle for mobile
-navDropdown.style.height = 'auto';     
-let dropdownHeight = navDropdown.clientHeight + 'px';
-navDropdown.style.removeProperty('height');
-btnDropdown.addEventListener('click', () => {
-  if (windowWidth < breakpoints.medium) {
-    if (!isActive(dropdownContainer)) {
-      dropdownContainer.classList.add('active');
+dropdownMobile.style.height = 'auto';     
+let dropdownMobileHeight = dropdownMobile.clientHeight + 'px';
+dropdownMobile.style.removeProperty('height');
+btnDropdownMobile.addEventListener('click', () => {
+  if (!isActive(dropdownMobile)) {
+    dropdownMobile.classList.add('active');
 
-      let transitionNumber = lastDropdownToggle.length;
-      lastDropdownToggle.push(transitionNumber);
+    let transitionNumber = lastDropdownToggle.length;
+    lastDropdownToggle.push(transitionNumber);
 
-      navDropdown.style.transition = 'height ' + (200 + 'ms') + ' ease-in-out';
-  
-      setTimeout( () => {
-        navDropdown.style.height = dropdownHeight;
-      }, 0);
-      setTimeout(() => {
-        if (isActive(dropdownContainer) && lastDropdownToggle[lastDropdownToggle.length - 1] == transitionNumber) {
-          navDropdown.style.removeProperty('transition');
-        }
-      }, 200);
-    } else {
-      dropdownContainer.classList.remove('active');
-      DropdownToggle(navDropdown, 200);
-    }
+    dropdownMobile.style.transition = 'height ' + (200 + 'ms') + ' ease-in-out';
+
+    setTimeout( () => {
+      dropdownMobile.style.visibility = "visible";
+      dropdownMobile.style.height = dropdownMobileHeight;
+    }, 0);
+    setTimeout(() => {
+      if (isActive(dropdownMobile) && lastDropdownToggle[lastDropdownToggle.length - 1] == transitionNumber) {
+        dropdownMobile.style.removeProperty('transition');
+      }
+    }, 200);
+  } else {
+    dropdownMobile.classList.remove('active');
+    DropdownToggle(dropdownMobile, 200);
   }
 });
 
 // dropdown toggle for desktop
-btnDropdown.addEventListener('mouseenter', () => {
-  if (windowWidth >= breakpoints.medium) {
-    if (!isActive(dropdownContainer)) {
-      dropdownContainer.classList.add('active');
-      DropdownToggle(navDropdown, 200);
-      DropdownContentToggle(dropdownContent, 200);
-    }
+dropdownDesktop.style.height = 'auto';   
+let dropdownDesktopHeight = dropdownDesktop.offsetHeight + 'px';
+dropdownDesktop.style.removeProperty('height'); 
+
+// open dropdown when hovered
+btnDropdownDesktop.addEventListener('mouseenter', () => {
+  if (!isActive(dropdownDesktop)) {
+    dropdownDesktop.classList.add('active');
+    DropdownToggle(dropdownDesktop, 200);
   }
 });
 
-dropdownContainer.addEventListener('mouseleave', () => {
-  if (windowWidth >= breakpoints.medium) {
-    if (isActive(dropdownContainer)) {
-      dropdownContainer.classList.remove('active');
-      DropdownToggle(navDropdown, 200);
-      DropdownContentToggle(dropdownContent, 200);
-    }
+// close dropdown when mouse leave
+dropdownDesktopContainer.addEventListener('mouseleave', () => {
+  if (isActive(dropdownDesktop)) {
+    dropdownDesktop.classList.remove('active');
+    DropdownToggle(dropdownDesktop, 200);
   }
 });
 
-window.addEventListener('mouseover', (event) => {
-  if (windowWidth >= breakpoints.medium) {
-    if (isActive(dropdownContainer) && event.target == navDropdown) {
-      dropdownContainer.classList.remove('active');
-      DropdownToggle(navDropdown, 200);
-      DropdownContentToggle(dropdownContent, 200);
-    }
+// open dropdown when focused
+dropdownDesktopContainer.addEventListener('focusin', () => {
+  if (!isActive(dropdownDesktop)) {
+    dropdownDesktop.classList.add('active');
+    DropdownToggle(dropdownDesktop, 200);
   }
 });
 
-let lastDropdownToggle = [];
+// close dropdown when focus out
+dropdownDesktopContainer.addEventListener('focusout', () => {
+  if (isActive(dropdownDesktop)) {
+    dropdownDesktop.classList.remove('active');
+    DropdownToggle(dropdownDesktop, 200);
+  }
+});
+
+let lastDropdownToggle = []; 
 function DropdownToggle(target, duration=300) {
-  if (isActive(dropdownContainer) && windowWidth >= breakpoints.medium) {
+  if (isActive(target)) {
     let transitionNumber = lastDropdownToggle.length;
     lastDropdownToggle.push(transitionNumber);
-    target.style.transition = 'height ' + (duration + 'ms') + ' ease-in-out';
+    setTimeout(() => {
+      target.style.transition = 'height ' + (duration + 'ms') + ' ease-in-out';
+    }, 0);
     setTimeout( () => {
-      target.style.height = '100%';
+      target.style.visibility = "visible";
+      target.style.height = dropdownDesktopHeight;
     }, 0);
     setTimeout(() => {
-      if (isActive(dropdownContainer) && lastDropdownToggle[lastDropdownToggle.length - 1] == transitionNumber) {
+      if (isActive(target) && lastDropdownToggle[lastDropdownToggle.length - 1] == transitionNumber) {
         target.style.removeProperty('transition');
       }
     }, duration);
@@ -264,56 +230,10 @@ function DropdownToggle(target, duration=300) {
     target.style.transition = 'height ' + (duration + 'ms') + ' ease-in-out';
     target.style.height = '0px';
     setTimeout( () => {
-      if (!isActive(dropdownContainer) && lastDropdownToggle[lastDropdownToggle.length - 1] == transitionNumber) {
+      if (!isActive(target) && lastDropdownToggle[lastDropdownToggle.length - 1] == transitionNumber) {
         target.style.removeProperty('transition');
         target.style.removeProperty('height');
-      }
-    }, duration);
-  }
-}
-
-dropdownContent.style.height = 'auto';     
-let dropdownContentHeight = dropdownContent.clientHeight + 'px';
-let dropdownContentPadding = {
-  top: window.getComputedStyle(dropdownContent).getPropertyValue('padding-top'),
-  bottom: window.getComputedStyle(dropdownContent).getPropertyValue('padding-bottom')
-}
-dropdownContent.style.removeProperty('height');
-function DropdownContentToggle(target, duration=300) {
-  if (isActive(dropdownContainer)) {
-    let transitionNumber = lastDropdownToggle.length - 1;
-    target.style.height = '0px';
-    target.style.paddingTop = '0px';
-    target.style.paddingBottom = '0px';
-    setTimeout(() => {
-      target.style.transitionProperty = 'height, padding';
-      target.style.transitionDuration = duration + 'ms';
-      target.style.transitionTimingFunction = 'ease-in-out';
-    }, 0);
-    setTimeout( () => {
-      target.style.height = dropdownContentHeight;
-      target.style.paddingTop = dropdownContentPadding.top;
-      target.style.paddingBottom = dropdownContentPadding.bottom;
-    }, 0);
-    setTimeout(() => {
-      if (isActive(dropdownContainer) && lastDropdownToggle[lastDropdownToggle.length - 1] == transitionNumber) {
-        target.style.removeProperty('transition');
-      }
-    }, duration);
-  } else {
-    let transitionNumber = lastDropdownToggle.length - 1;
-    target.style.transitionProperty = 'height, padding';
-    target.style.transitionDuration = duration + 'ms';
-    target.style.transitionTimingFunction = 'ease-in-out';
-    target.style.height = '0px';
-    target.style.paddingTop = '0px';
-    target.style.paddingBottom = '0px';
-    setTimeout( () => {
-      if (!isActive(dropdownContainer) && lastDropdownToggle[lastDropdownToggle.length - 1] == transitionNumber) {
-        target.style.removeProperty('transition');
-        target.style.removeProperty('height');
-        target.style.removeProperty('padding-top');
-        target.style.removeProperty('padding-bottom');
+        target.style.visibility = "hidden";
       }
     }, duration);
   }
