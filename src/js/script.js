625981332a772a12991ae8addf875902a20aaab1
Rawthul <ozgurgokd@gmail.com>
2021-12-03 21:20:13 +0300

    product page structure

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,4 @@
-//#region => GLOBAL 
-
+//#region => HELPER 
 const fontSize = window.getComputedStyle(document.documentElement).getPropertyValue('font-size').replace('px', '');
 
 const breakpoints = {
@@ -34,7 +33,86 @@ let TargetHeight = (target) => {
   target.style.removeProperty('height');
   return height;
 }
+//#endregion
 
+//#region => CLASSES 
+class Dropdown {
+  constructor(container, button, hover, dropdown, height, chevron) {
+    this.container = container;
+    this.button = button;
+    this.hover = hover;
+    this.dropdown = dropdown;
+    this.height = height;
+    this.chevron = chevron;
+  }
+  toggle(duration = 200) {
+    this.container.classList.toggle('active');
+    this.button?.classList.toggle('active');
+    this.hover?.classList.toggle('active');
+    this.chevron?.forEach(element => element.classList.toggle('active'));
+    if (IsActive(this.container)) {
+      this.dropdown.style.visibility = "visible";
+      this.dropdown.style.height = this.height;
+    } else {
+      this.dropdown.style.height = this.height;
+      setTimeout(() => {
+        this.dropdown.style.height = '0px';
+      }, 0);
+      setTimeout( () => {
+        if (!IsActive(this.container)) {
+          this.dropdown.style.visibility = "hidden";
+        }
+      }, duration);
+    }
+  }
+}
+//#endregion
+
+//#region => PROPERTIES 
+const hamburgerButton = document.querySelector(".js-hamburger-toggle");
+const hamburgerMenu = document.querySelector(".js-hamburger-menu");
+
+const dropdowns = [];
+document.querySelectorAll(".js-dropdown-container").forEach(element => {
+  dropdowns.push(new Dropdown(
+    element,
+    element.querySelector(".js-dropdown-button"),
+    element.querySelector(".js-dropdown-hover"),
+    element.querySelector(".js-dropdown"),
+    TargetHeight(element.querySelector(".js-dropdown")),
+    element.querySelectorAll(".chevron")
+  ))
+});
+//#endregion
+
+//#region => FUNCTIONS 
+let HamburgerToggle = (target, duration=300) => {
+  bars = target.querySelectorAll('span');
+  if (IsActive(hamburgerMenu)) {
+    bars[0].style.top = 'calc(50% - ' + 3.6 / fontSize + 'rem / 2)';
+    bars[1].style.opacity = '0';
+    bars[2].style.top = 'calc(50% - ' + 3.6 / fontSize + 'rem / 2)';
+    window.setTimeout(() => {
+      if (IsActive(hamburgerMenu)) {
+        bars[0].style.transform = 'rotate(-45deg)';
+        bars[2].style.transform = 'rotate(45deg)';
+      }
+    }, duration / 2);
+  } else {
+    bars[0].style.transform = 'rotate(0deg)';
+    bars[2].style.transform = 'rotate(0deg)';
+    window.setTimeout(() => {
+      if (!IsActive(hamburgerMenu)) {
+        bars[0].style.top = '1.25rem';
+        bars[1].style.opacity = '1';
+        bars[2].style.top = 'calc(100% - ' + 3.6 / fontSize + 'rem - 1.25rem)';
+      }
+    }, duration / 2);
+  }
+}
+//#endregion
+
+//#region => GLOBAL 
 let ResetStyles = () => {
   // update current media
   currentMedia = IsMediaMobile();
@@ -84,10 +162,6 @@ window.addEventListener('click', (event) => {
 //#endregion
 
 //#region => NAVIGATION MENU 
-
-const hamburgerButton = document.querySelector(".js-hamburger-toggle");
-const hamburgerMenu = document.querySelector(".js-hamburger-menu");
-
 if (IsMediaMobile) hamburgerMenu.style.transition = 'transform 300ms ease-in-out';
 
 // toggle menu when clicked to hamburger button
@@ -96,79 +170,9 @@ hamburgerButton.addEventListener('click', () => {
   body.classList.toggle('noscroll');
   HamburgerToggle(hamburgerButton, 300);
 });
-
-function HamburgerToggle(target, duration=300) {
-  bars = target.querySelectorAll('span');
-  if (IsActive(hamburgerMenu)) {
-    bars[0].style.top = 'calc(50% - ' + 3.6 / fontSize + 'rem / 2)';
-    bars[1].style.opacity = '0';
-    bars[2].style.top = 'calc(50% - ' + 3.6 / fontSize + 'rem / 2)';
-    window.setTimeout(() => {
-      if (IsActive(hamburgerMenu)) {
-        bars[0].style.transform = 'rotate(-45deg)';
-        bars[2].style.transform = 'rotate(45deg)';
-      }
-    }, duration / 2);
-  } else {
-    bars[0].style.transform = 'rotate(0deg)';
-    bars[2].style.transform = 'rotate(0deg)';
-    window.setTimeout(() => {
-      if (!IsActive(hamburgerMenu)) {
-        bars[0].style.top = '1.25rem';
-        bars[1].style.opacity = '1';
-        bars[2].style.top = 'calc(100% - ' + 3.6 / fontSize + 'rem - 1.25rem)';
-      }
-    }, duration / 2);
-  }
-}
 //#endregion
 
 //#region => DROPDOWN 
-
-class Dropdown {
-  constructor(container, button, hover, dropdown, height, chevron) {
-    this.container = container;
-    this.button = button;
-    this.hover = hover;
-    this.dropdown = dropdown;
-    this.height = height;
-    this.chevron = chevron;
-  }
-  toggle(duration = 200) {
-    this.container.classList.toggle('active');
-    this.button?.classList.toggle('active');
-    this.hover?.classList.toggle('active');
-    this.chevron?.forEach(element => element.classList.toggle('active'));
-    if (IsActive(this.container)) {
-      this.dropdown.style.visibility = "visible";
-      this.dropdown.style.height = this.height;
-    } else {
-      this.dropdown.style.height = this.height;
-      setTimeout(() => {
-        this.dropdown.style.height = '0px';
-      }, 0);
-      setTimeout( () => {
-        if (!IsActive(this.container)) {
-          this.dropdown.style.visibility = "hidden";
-        }
-      }, duration);
-    }
-  }
-}
-
-const dropdowns = [];
-
-document.querySelectorAll(".js-dropdown-container").forEach(element => {
-  dropdowns.push(new Dropdown(
-    element,
-    element.querySelector(".js-dropdown-button"),
-    element.querySelector(".js-dropdown-hover"),
-    element.querySelector(".js-dropdown"),
-    TargetHeight(element.querySelector(".js-dropdown")),
-    element.querySelectorAll(".chevron")
-  ))
-});
-
 dropdowns.forEach(element => {
   if (element.button != undefined) {
     // toggle dropdown when clicked
