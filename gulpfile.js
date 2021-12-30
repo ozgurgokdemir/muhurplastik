e4c7741edc34479c2f2b5374c93660415fcf2b20
Rawthul <ozgurgokd@gmail.com>
2021-12-30 06:45:21 +0300

    Update gulp & styling

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 const { task, src, dest, watch, series, parallel } = require("gulp");
 
+const htmlmin = require("gulp-htmlmin");
+
 const sass = require("gulp-sass")(require("sass"));
 const postcss = require("gulp-postcss");
-const purge = require("gulp-purgecss");
 const prefix = require("autoprefixer");
 const minify = require("cssnano");
 
@@ -20,37 +21,26 @@ const webpack = require("webpack");
 const webpackStream = require("webpack-stream");
 const webpackConfig = require("./webpack.config.js");
 
-function scssTask() {
+task("compileHTML", () => {
+	return src("src/**/*.html")
+		.pipe(htmlmin({ collapseWhitespace: true }))
+		.pipe(dest("dist"));
+});
+
+task("compileSASS", () => {
 	return src("src/scss/*.scss", { sourcemaps: true })
 		.pipe(sass())
-		.pipe(purge({ content: ["./*.html"] }))
 		.pipe(postcss([prefix(), minify()]))
 		.pipe(dest("dist/css", { sourcemaps: "." }));
-}
+});
 
-function jsTask() {
+task("compileJS", () => {
 	return src("src/js/main.js", { sourcemaps: true })
 		.pipe(webpackStream(webpackConfig), webpack)
 		.pipe(dest("dist/js", { sourcemaps: "." }));
-}
+});
 
-function convertToWebp() {
-	const image = (size) => {
-		return src("src/images/*.png")
-			.pipe(imageResize({ imageMagick: true, width: size }))
-			.pipe(imagemin([imageminWebp({ quality: 70 })]))
-			.pipe(
-				rename({
-					suffix: size !== null ? `_${size}w` : "",
-					extname: ".webp",
-				})
-			)
-			.pipe(dest("dist/images"));
-	};
-	return merge([480, 960].map(image));
-}
-
-function optimizePNG() {
+task("optimizePNG", () => {
 	return src("src/images/*.png")
 		.pipe(imageResize({ imageMagick: true, width: 960 }))
 		.pipe(
@@ -62,9 +52,9 @@ function optimizePNG() {
 			])
 		)
 		.pipe(dest("dist/images"));
-}
+});
 
-function optimizeSVG() {
+task("optimizeSVG", () => {
 	return src("src/images/*.svg")
 		.pipe(
 			imagemin([
@@ -78,32 +68,56 @@ function optimizeSVG() {
 			])
 		)
 		.pipe(dest("dist/images"));
-}
+});
 
-function browsersyncServe(callback) {
+task("convertToWebp", () => {
+	const image = (size) => {
+		return src("src/images/*.png")
+			.pipe(imageResize({ imageMagick: true, width: size }))
+			.pipe(imagemin([imageminWebp({ quality: 70 })]))
+			.pipe(
+				rename({
+					suffix: size !== null ? `_${size}w` : "",
+					extname: ".webp",
+				})
+			)
+			.pipe(dest("dist/images"));
+	};
+	return merge([480, 960].map(image));
+});
+
+task("browsersyncServe", (callback) => {
 	browsersync.init(
 		{
 			server: {
-				baseDir: ".",
+				baseDir: "./dist/",
 			},
 		},
 		callback
 	);
-}
-function browsersyncReload(callback) {
+});
+
+task("browsersyncReload", (callback) => {
 	browsersync.reload();
 	callback();
-}
+});
 
-function watchTask() {
+task("watch", () => {
 	watch(
-		["*.html", "src/js/**/*.js", "src/scss/**/*.scss", "src/images/*"],
-		parallel(jsTask, scssTask, browsersyncReload)
+		["src/**/*.html", "src/scss/**/*.scss", "src/js/**/*.js"],
+		parallel("base", "browsersyncReload")
 	);
-}
+});
 
-exports.default = series(
-	parallel(jsTask, scssTask, optimizePNG, optimizeSVG, convertToWebp),
-	browsersyncServe,
-	watchTask
+task(
+	"image",
+	parallel("optimizePNG", "optimizeSVG", "convertToWebp", () => {
+		return src("src/favicon.ico").pipe(dest("dist"));
+	})
 );
+
+task("base", parallel("compileHTML", "compileSASS", "compileJS"));
+
+task("default", series("base", "browsersyncServe", "watch"));
+
+task("build", parallel("base", "image"));
